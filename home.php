c4ee5046483a157a8f9a5f34949bac362cacf560
pooja <147916337+poojakumarigithub@users.noreply.github.com>
2025-03-26 12:56:26 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+<!--Font awesome link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<!--Custom css link-->
+<link rel="stylesheet" href="css/style.css">
+<!--swiper css link-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+
+</head>
+<body>
+    <!--Header section start-->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+<!--Header section end-->
+
+<!--Home section start-->
+<section class="home">
+
+    <div class=" swiper home-slider">
+
+    <div class="swiper-wrapper">
+
+    <div class=" swiper-slide slide" style="background: url(images/home1.jpg) no-repeat;">
+        <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>travel around the world</h3>
+            <a href="package.php" class="btn">discover more</a>
+        </div>
+    </div>
+
+    <div class=" swiper-slide slide" style="background: url(images/home2.jpg) no-repeat;">
+        <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>discover the new  places</h3>
+            <a href="package.php" class="btn">discover more</a>
+        </div>
+    </div>
+
+    <div class=" swiper-slide slide" style="background: url(images/home3.jpg) no-repeat;">
+        <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>make your tour worthwhile</h3>
+            <a href="package.php" class="btn">discover more</a>
+        </div>
+    </div>
+
+    </div>
+
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+
+    </div>
+
+</section>
+<!--Home section end-->
+
+
+
+
+
+
+
+<!--services section end-->
+<section class="services">
+
+    <h1 class="heading-title"> our services </h1>
+    <div class="box-container">
+
+    <div class="box">
+        <img src="images/icon-1.png" alt="">
+        <h3>adventure</h3>
+    </div>
+
+    <div class="box">
+        <img src="images/icon-2.png" alt="">
+        <h3>tour guide</h3>
+    </div>
+
+    <div class="box">
+        <img src="images/icon-3.png" alt="">
+        <h3>trekking</h3>
+    </div>
+
+    <div class="box">
+        <img src="images/icon-4.png" alt="">
+        <h3>camp fire</h3>
+    </div>
+
+    <div class="box">
+        <img src="images/icon-5.png" alt="">
+        <h3>off road</h3>
+    </div>
+
+    <div class="box">
+        <img src="images/icon-6.png" alt="">
+        <h3>camping</h3>
+    </div>
+
+    </div>
+
+</section>
+<!--services section end-->
+
+<!--home about  section start-->
+<section class="home-about">
+
+<div class="im">
+         <img src="images/about.jpg">
+    </div>
+
+    <div class="content">
+        <h3>about us</h3>    
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Fugiat maxime labore vero odio libero quam, 
+        dignissimos odit voluptatum. Provident id voluptates aliquid consectetur autem nisi
+        odio excepturi? Saepe, perspiciatis magnam.</p>
+        <a href="about.php" class="btn">read more</a>
+    </div>
+
+</section>
+<!--home about section end-->
+
+<!--home packages section start-->
+<section class="home-packages">
+
+    <h1 class="heading-title">our packages</h1>
+<div class="box-container">
+
+    <div class="box">
+        <div class="image">
+            <img src="images/img1.jpg">
+        </div>
+
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, dolorum!</p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="images/img2.jpg">
+        </div>
+
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, dolorum!</p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="image">
+            <img src="images/img3.jpg">
+        </div>
+
+        <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, dolorum!</p>
+            <a href="book.php" class="btn">book now</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="load-more" ><a href="package.php" class="btn">load more</a></div>
+
+</section>
+<!--home packages section end-->
+
+<!--home offer section start-->
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, facilis earum
+         impedit doloremque distinctio vel perferendis culpa. Atque, rem soluta!</p>
+        <a href="book.php" class="btn">book now</a>
+        
+    </div>
+</section>
+<!--home offer section end-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--footer section start-->
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+        <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+        <a href="#"><i class="fas fa-phone"></i> +123-341-2221 </a>
+        <a href="#"><i class="fas fa-phone"></i> +123-341-4441 </a>
+        <a href="#"><i class="fas fa-envelope"></i>p12@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i> Haryana,India </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            <a href="#"><i class="fab fa-facebook"></i>facebook</a>
+        </div>
+
+
+    </div>
+
+    <div class="credit"> created by <span>miss.pooja</span> | all rights reserved!</div>
+</section>
+<!--footer section end-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<!--script link-->
+<script src="js/script.js"></script>
+
+</body>
+</html>
